887efaca414e0625d85164faf0aedbe16db32536
Brian Binder <bbinder912@gmail.com>
2017-10-21 17:31:42 -0700

    Create rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,5 @@
 // YOUR CODE HERE:
+window.messageElements = [];
 
 var app = {
   server: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
@@ -6,7 +7,6 @@ var app = {
   roomname: 'lobby',
   messages: [],
   lastMessageId: 0,
-
 
   init: function() {
     app.username = window.location.search.slice(10);
@@ -24,6 +24,12 @@ var app = {
     $('#refresh').on('click', function() {
       app.fetch();
     });
+    $('#rooms').change(function() {
+      app.changeRoom();
+    });
+    $('#roomSubmit').on('click', function() {
+      app.renderRoom();
+    });
   },
 
   fetch: function() {
@@ -34,7 +40,7 @@ var app = {
       success: function(data) {
         app.messages = data.results;
         if (app.messages[app.messages.length - 1].objectId !== app.lastMessageId) {
-          loadMessages();
+          app.renderMessage();
         }
       },
       error: function(data) {
@@ -62,12 +68,108 @@ var app = {
         console.error('chatterbox: Failed to send message', data);
       }
     });
-  }
+  },
 
+  changeRoom: function() {
+    app.roomname = $('#rooms').val();
+    $.ajax({
+      url: app.server,
+      data: 'where=' + JSON.stringify({roomname: app.roomname}),
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Messages received');
+        app.messages = data.results;
+        app.renderMessage();
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to receive message', data);
+      }
+    });
+  },
+
+  renderRoom: function() {
+    var newRoom = $('#newRoom').val();
+    app.roomname = newRoom;
+    $('#rooms').append($('<option></option>').val(newRoom).html(newRoom));
+    $('#rooms').val(newRoom);
+    $('#newRoom').val('');
+    app.changeRoom();
+  },
+
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+
+  renderMessage: function() {
+    $('#chats').empty();
+    window.rooms = [];
+    for (let i = 0; i < app.messages.length; i++) {
+      messageElements[i] = new MakeMessage(app.messages[i]);
+      $('#chats').append(messageElements[i].$node);
+      if (rooms.indexOf(messageElements[i].roomname) === -1) {
+        rooms.push(messageElements[i].roomname);
+      }
+      $(messageElements[i].$node).children('a').on('click', function() {
+        var user = messageElements[i].user;
+        makeFriend(user);
+        console.log('clicked ' + messageElements[i].user + '\'s username');
+      });
+    }
+    $('#rooms').empty();
+    rooms.forEach(function(room) {
+      $('#rooms').append($('<option></option>').val(room).html(room));
+    });
+  }
 };
 
-window.messageElements = [];
-$(document).ready(function() {
+
+
+var MakeMessage = function(message) {
+  this.user = DOMPurify.sanitize(message.username, {ALLOWED_TAGS: ['b']});
+  this.text = DOMPurify.sanitize(message.text, {ALLOWED_TAGS: ['b']});
+  this.roomname = DOMPurify.sanitize(message.roomname, {ALLOWED_TAGS: ['b']});
+  this.$node = $('<div class="chat"></div>');
+  this.$node.append('<a class="chat username ' + this.user + '">' + this.user + '</a>');
+  this.$node.append('<p>' + this.text + '</p>');
+};
+
+var makeFriend = function(user) {
+  var messageNode;
+  for (let i = 0; i < messageElements.length; i++) {
+    messageNode = messageElements[i].$node;
+    if (messageElements[i].user === user) {
+      if (messageNode.hasClass('friend')) {
+        messageNode.removeClass('friend');
+      } else {
+        messageNode.addClass('friend');
+      }
+    }
+  }
+};
+// var loadMessages = function() {
+//   $('#chats').empty();
+//   window.rooms = [];
+//   for (let i = 0; i < app.messages.length; i++) {
+//     messageElements[i] = new MakeMessage(app.messages[i]);
+//     $('#chats').append(messageElements[i].$node);
+//     if (rooms.indexOf(messageElements[i].roomname) === -1) {
+//       rooms.push(messageElements[i].roomname);
+//     }
+//     $(messageElements[i].$node).children('a').on('click', function() {
+//       var user = messageElements[i].user;
+//       makeFriend(user);
+//       console.log('clicked ' + messageElements[i].user + '\'s username');
+//     });
+//   }
+//   $('#rooms').empty();
+//   rooms.forEach(function(room) {
+//     $('#rooms').append($('<option></option>').val(room).html(room));
+//   });
+// };
+
+
+// $(document).ready(function() {
   //window.messages;
 
   //requestMessages();
@@ -99,32 +201,29 @@ $(document).ready(function() {
   //   });
   // });
 
-  $('#rooms').change(function() {
-    app.roomname = $('#rooms').val();
-    $.ajax({
-      url: app.server,
-      data: 'where=' + JSON.stringify({roomname: app.roomname}),
-      type: 'GET',
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Messages received');
-        app.messages = data.results;
+  // $('#rooms').change(function() {
+  //   app.roomname = $('#rooms').val();
+  //   $.ajax({
+  //     url: app.server,
+  //     data: 'where=' + JSON.stringify({roomname: app.roomname}),
+  //     type: 'GET',
+  //     contentType: 'application/json',
+  //     success: function (data) {
+  //       console.log('chatterbox: Messages received');
+  //       app.messages = data.results;
 
-        loadMessages();
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to receive message', data);
-      }
-    });
-  });
+  //       loadMessages();
+  //     },
+  //     error: function (data) {
+  //       console.error('chatterbox: Failed to receive message', data);
+  //     }
+  //   });
+  // });
 
   // $('#refresh').on('click', function() {
   //   requestMessages();
   // });
-});
-
-
-
+// });
 // var requestMessages = function() {
 //   $('#chats').empty();
 //   $.ajax({
@@ -141,43 +240,3 @@ $(document).ready(function() {
 //     }
 //   });
 // }
-
-var loadMessages = function() {
-  $('#chats').empty();
-  window.rooms = [];
-  for (let i = 0; i < app.messages.length; i++) {
-    messageElements[i] = new MakeMessage(app.messages[i]);
-    $('#chats').append(messageElements[i].$node);
-    if (rooms.indexOf(messageElements[i].roomname) === -1) {
-      rooms.push(messageElements[i].roomname);
-    }
-    $(messageElements[i].$node).children('a').on('click', function() {
-      var user = messageElements[i].user;
-      makeFriend(user);
-      console.log('clicked ' + messageElements[i].user + '\'s username');
-    });
-  }
-  $('#rooms').empty();
-  rooms.forEach(function(room) {
-    $('#rooms').append($('<option></option>').val(room).html(room));
-  });
-};
-
-var MakeMessage = function(message) {
-  this.user = DOMPurify.sanitize(message.username, {ALLOWED_TAGS: ['b']});
-  this.text = DOMPurify.sanitize(message.text, {ALLOWED_TAGS: ['b']});
-  this.roomname = DOMPurify.sanitize(message.roomname, {ALLOWED_TAGS: ['b']});
-  this.$node = $('<div class="chat"></div>');
-  this.$node.append('<a class="chat username ' + this.user + '">' + this.user + '</a>');
-  this.$node.append('<p>' + this.text + '</p>');
-};
-
-var makeFriend = function(user) {
-  for (let i = 0; i < messageElements.length; i++) {
-    if (messageElements[i].user === user) {
-      messageElements[i].$node.addClass('friend');
-    }
-  }
-};
-
-
